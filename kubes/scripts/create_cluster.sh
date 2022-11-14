93230992c7cd8ae6deb68e823917b7f3cb48afd0
Marshall Conover <marshall.conover@ansys.com>
2022-11-14 16:40:14 -0500

    Correct pathing for shellscript call.

--- kubes/scripts/create_cluster.sh
+++ kubes/scripts/create_cluster.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 # NOTE: Script assumes you've installed kubectl
 # NOTE: RUN THIS IN THE TOP LEVEL OF THIS REPO
 
@@ -7,7 +8,7 @@ go install sigs.k8s.io/kind@v0.17.0
 # Installs an nginx ingress handler on the cluster. See: https://kind.sigs.k8s.io/docs/user/ingress/
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
-kind create cluster --config ../cluster-config.yaml
+kind create cluster --config kubes/cluster-config.yaml
 docker build -t server:newer .
 kind load docker-image server:newer
 kubectl wait --namespace ingress-nginx   --for=condition=ready pod   --selector=app.kubernetes.io/component=controller   --timeout=90s
